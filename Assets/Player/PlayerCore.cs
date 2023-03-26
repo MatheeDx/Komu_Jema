68bbe12618eb712fec0f7d8b9fd596d055cccdeb
Max <maximc00222@gmail.com>
2023-03-26 14:08:42 +0300

    Player and nedoded

--- Assets/Player/PlayerCore.cs
+++ Assets/Player/PlayerCore.cs
@@ -6,14 +6,19 @@ public class PlayerCore : MonoBehaviour
 {
     [SerializeField] float speedMove;
     [SerializeField] float speedRot;
+    float joy;
+    bool isMoving;
+    bool isUsing;
     Item activeItem;
 
-    void Update()
+    void FixedUpdate()
     {
-        MoveControl(true);
+        MoveControl(isMoving);
+        //Crouch(isUsing);
 
-        if(Input.GetButtonDown("Use"))
-            ItemUse();
+        if (Input.GetButtonDown("Use"))
+            StartCoroutine(ItemUse());
+        
     }
 
     private void OnTriggerEnter(Collider other)
@@ -36,28 +41,65 @@ public class PlayerCore : MonoBehaviour
 
     void MoveControl(bool isMove)
     {
-        float joy = Input.GetAxis("Horizontal");
+        joy = Input.GetAxis("Horizontal");
 
         if (joy != 0 && isMove)
         {
+            anim.SetBool("isWalking", true);
             _transform.position += new Vector3(joy * 0.1f * speedMove, 0, 0);
             if (joy < 0)
                 _transform.rotation = Quaternion.Lerp(_transform.rotation, Quaternion.LookRotation(new Vector3(-1, 0, -0.01f), Vector3.up), speedRot * Time.deltaTime);
             else
                 _transform.rotation = Quaternion.Lerp(_transform.rotation, Quaternion.LookRotation(new Vector3(1, 0, -0.01f), Vector3.up), speedRot * Time.deltaTime);
+        } else {
+            anim.SetBool("isWalking", false);
         }
     }
 
-    void ItemUse()
+    private IEnumerator ItemUse()
     {
-        if (activeItem != null)
+        if (activeItem != null && !isUsing)
+        {
+            anim.SetTrigger("isUsing");
+            isMoving = false;
+            isUsing = true;
+            yield return new WaitForSeconds(1.5f);
             activeItem.Use();
+            isMoving = true;
+            isUsing = false;
+        }
     }
+
+    //private void Crouch(bool isCrouch)
+    //{
+    //    if (!isCrouch) { 
+    //        if (Input.GetButton("Crouch"))
+    //        {
+    //            anim.SetBool("isCrouching", true);
+    //            anim.SetBool("isWalking", false);
+    //            if (joy != 0)
+    //                anim.StopPlayback();
+    //            else
+    //                anim.StartPlayback();
+    //        }
+    //        if (Input.GetButtonUp("Crouch"))
+    //        {
+    //            anim.SetBool("isCrouching", false);
+    //            anim.SetBool("isWalking", true);
+    //        }
+            
+    //    }
+    //}
+
     private void Awake()
     {
         _transform = GetComponent<Transform>();
+        anim = GetComponent<Animator>();
+        isMoving = true;
+        isUsing = false;
         activeItem = null;
     }
 
+    Animator anim;
     Transform _transform;
 }
